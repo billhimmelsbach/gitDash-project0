5a378922b59cdf513e4b6bc0a14fd97d9b9eee6f
billhimmelsbach <whimmels@gmail.com>
2016-07-09 08:53:18 -0700

    adds an image for runner, a listener for keyup on z, and a basic move right based on changing the margin left

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
-console.log("Am I sane?");
-var track = $('.player1Track');
+$('document').ready(function() {
+// console.log("Am I sane?");
+// var track = $('.player1Track');
+// // $('')
+//
+// playerRow = $('.player1Track');
+//
+//
+//
+//   // td.cellIndex)
+// console.log(track[0]);
+function PlayerCreate(number,name,counter) {
+  this.number=number;
+  this.number.name=name;
+  this.number.counter=counter;
+}
+PlayerCreate(1,bill,200);
+$('body').on('keyup', function(event) {
+  if(event.which==90) {
+    $('.player1Track').css({marginLeft: '+=0.5%'});
+    counter++;
+    if (counter===10) {
+      alert("you win!");
+    }
+    // console.log($('.player1Track').css("marginLeft"));
+    // if (($('.player1Track').css({marginLeft})) == 100%) {
+      // console.log("WIN!");
+    console.log("working!");
+  }
 
-console.log(track[0]);
+});
+});
