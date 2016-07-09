24294f278e9f6242914b90b281e89ad8060a8a82
billhimmelsbach <whimmels@gmail.com>
2016-07-09 09:20:00 -0700

    adds basic player1 object

--- js/app.js
+++ js/app.js
@@ -9,18 +9,34 @@ $('document').ready(function() {
 //
 //   // td.cellIndex)
 // console.log(track[0]);
+function Flower(name, color, petals, edible, medicinal, lifespan, genus, season, image) {
+  this.name = name;
+  this.color = color; //string
+  this.petals = petals;  //number
+  this.edible = edible;
+  this.medicinal = medicinal;
+  this.lifespan = lifespan;
+  this.genus = genus;
+  this.season = season;
+  this.image = image;
+  // this.render = render();
+}
+myFlower = new Flower("Rose", "red", 30, true, false, 30, "this damn rose", "fall", "http://pngimg.com/upload/rose_PNG639.png");
+
 function PlayerCreate(number,name,counter,image) {
   this.number=number;
-  this.number.name=name;
-  this.number.counter=counter;
-  this.number.image=image
+  this.name=name;
+  this.counter=counter;
+  this.image=image;
 }
-PlayerCreate(1,bill,200);
+
+Player1 = new PlayerCreate("player1","bill",10);
+
 $('body').on('keyup', function(event) {
   if(event.which==90) {
     $('.player1Track').css({marginLeft: '+=0.5%'});
     counter++;
-    if (counter===10) {
+    if (Player1.number.counter===10) {
       alert("you win!");
     }
     // console.log($('.player1Track').css("marginLeft"));
