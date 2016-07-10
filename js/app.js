7b4194007e9100b9b698d220921a0ed0aae90757
billhimmelsbach <whimmels@gmail.com>
2016-07-09 17:33:18 -0700

    win conditions work for all players

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 $('document').ready(function() {
+  $('.overlay').hide();
 // var Players ={};
 // console.log("Am I sane?");
 
@@ -15,14 +16,14 @@ $('document').ready(function() {
 
 function postWin(playerWin) {
   console.log(playerWin + "WINS THE GAME!");
-  $('.playerWinText').css({visibility: visible});
+  $('.overlay').show();
   $('.playerWinText').html(playerWin + " WINS THE GAME");
 }
 
 //.css only returns widths in pixels (if the width is located in the CSS file instead of inline in the HTML)
 //this hack takes the pixel output and converts it into a percent
 function calculateWidthPercent(parentDiv) {
-  return (Math.floor((100-(( 100 * parseFloat($(parentDiv).css('width')) / parseFloat($(parentDiv).parent().css('width')) )))) + '%');
+  return (Math.floor((100-(( 100 * parseFloat($(parentDiv).css('width')) / parseFloat($('.raceBoard').css('width')) )))) + '%');
 }
 
 // var width = ((( 100 * parseFloat($(this).css('width')) / parseFloat($(this).parent().css('width')) )) + '%');
@@ -30,8 +31,8 @@ $('body').on('keyup', function(event) {
   if(event.which==49) {
     $('.player1Track').css({marginLeft: '+=1%'});
     console.log(calculateWidthPercent('.player1Track'));
-    var width = calculateWidthPercent('.player1Track');
-    if (width==="92%") {
+    var width2 = calculateWidthPercent('.player1Track');
+    if (width2>="90%") {
       postWin("Player 1");
     }
     console.log("working!");
@@ -42,19 +43,22 @@ $('body').on('keyup', function(event) {
   if(event.which===48) {
     $('.player2Track').css({marginLeft: '+=1%'});
     console.log(calculateWidthPercent('.player2Track'));
-    var width = calculateWidthPercent('.player2Track');
-    if (width==="60%") {
+    var width2 = calculateWidthPercent('.player2Track');
+    if (width2==="90%") {
       postWin("Player 2");
     }
   }
 });
 
 $('body').on('keyup', function(event) {
-  if(event.which==90) {
+  var width = calculateWidthPercent('.player3Track');
+  if (width>="90%") {
+  }
+  else if(event.which==90) {
     $('.player3Track').css({marginLeft: '+=1%'});
     console.log(calculateWidthPercent('.player3Track'));
-    var width = calculateWidthPercent('.player3Track');
-    if (width==="60%") {
+    var width2 = calculateWidthPercent('.player3Track');
+    if (width2==="90%") {
       postWin("Player 3");
     }
   }
@@ -64,8 +68,8 @@ $('body').on('keyup', function(event) {
   if(event.which==39) {
     $('.player4Track').css({marginLeft: '+=1%'});
     console.log(calculateWidthPercent('.player4Track'));
-    var width = calculateWidthPercent('.player4Track');
-    if (width==="60%") {
+    var width2 = calculateWidthPercent('.player4Track');
+    if (width2==="90%") {
       postWin("Player 4");
     }
   }
